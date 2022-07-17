e39d9f05fa94c8e82ce95c4da964c4ef2b0da35a
JunHwan Kim <nefertiri972@gmail.com>
2022-07-18 01:12:08 +0900

    Add file upload

--- src/Upload.js
+++ src/Upload.js
@@ -1,41 +1,58 @@
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Button, Form, Container } from 'react-bootstrap';
 
 const Upload = () => {
-  const handleFiles = e => {
-    console.log(e.target.files);
+  const [file, setFile] = useState(null);
+  const [upload, setUpload] = useState(false);
+
+  const handleFile = e => {
+    console.log(e.target.files[0]);
+    setFile(e.target.files[0]);
   };
 
-  const uploadFiles = e => {};
+  const uploadFile = async e => {
+    if (file === null) {
+      alert('파일을 등록해주세요');
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    console.log(formData);
+
+    setUpload(true);
+
+    const response = await axios.post(
+      'http://localhost:8080/upload',
+      formData,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      },
+    );
+    setFile(null);
+    setUpload(false);
+
+    console.log(response);
+  };
+
+  if (upload) {
+    return 'Uploading...';
+  }
 
   return (
     <Container>
       <Row>
         <Col>
           <Form encType="multipart/form-data">
-            <Form.Group controlId="formFileMultiple" className="mb-3">
-              <Form.Label>Directory</Form.Label>
-              <Form.Control
-                type="file"
-                name="files[]"
-                multiple=""
-                webkitdirectory=""
-                onChange={handleFiles}
-              />
-              <Button onClick={uploadFiles}>업로드</Button>
-            </Form.Group>
-          </Form>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Form>
-            <Form.Group controlId="formGithub" className="mb-3">
-              <Form.Label>GitHub</Form.Label>
-              <Form.Control type="text" name="github"></Form.Control>
-              <Button>제출</Button>
+            <Form.Group className="mb-3">
+              <Form.Label>File</Form.Label>
+              <Form.Control type="file" name="file" onChange={handleFile} />
+              <Button onClick={uploadFile}>업로드</Button>
             </Form.Group>
           </Form>
         </Col>
